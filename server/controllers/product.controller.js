1b828e0f5bc95de3cb2269329930b40782caf45e
LJR411~ <ljremulla@yahoo.com>
2023-08-11 17:37:07 -0700

    init commit

--- server/controllers/product.controller.js
+++ server/controllers/product.controller.js
@@ -18,3 +18,20 @@ module.exports.allProduct = (req, res)=>{
         .catch(err => res.json(err))
 }
 
+module.exports.viewProduct=(req,res)=>{
+    Product.findOne({_id: req.params.id})
+        .then(oneProduct => res.json(oneProduct))
+        .catch(err => res.json(err))
+}
+
+module.exports.editProduct=(req,res)=>{
+    Product.findOneAndUpdate({_id:req.params.id}, req.body, {new:true, runValidators:true})
+        .then(updatedProd => res.json(updatedProd))
+        .catch(err=>res.json(err))
+}
+
+module.exports.deleteProduct=(req,res)=>{
+    Product.deleteOne({_id:req.params.id})
+    .then(result => res.json(result))
+    .catch(err => res.json(err))
+}
--- server/routes/product.routes.js
+++ server/routes/product.routes.js
@@ -4,7 +4,10 @@ const ProductController = require("../controllers/product.controller")
 const routes = (app) =>{
     app.get('/api/testing', ProductController.apiTest)
     app.get('/api/product/all',ProductController.allProduct)
+    app.get('/api/product/view/:id', ProductController.viewProduct)
     app.post('/api/product/create', ProductController.newProduct)
+    app.patch('/api/product/edit/:id',ProductController.editProduct)
+    app.delete('/api/product/delete/:id', ProductController.deleteProduct)
 }
 // 3. include all the routes with the corresponding logic from controller (test)
 
